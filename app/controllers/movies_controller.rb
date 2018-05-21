c96970e630a0860cf4212646ab187c5e33533851
github username <github email address>
2018-05-21 15:33:42 -0400

    structure

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,53 @@
 class MoviesController < ApplicationController
+
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movies = Movie.all.order("created_at DESC")
   end
+
+  def show
+
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    #show page does not exist because the movie is gone, thus redirecting to root path
+    redirect_to root_path
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :description, :director, :year)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:id])
+    end
 end
