f8a57ae7dbb149e4622b7b26de2527618968ed45
github username <github email address>
2018-06-12 22:23:52 -0400

    review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  before_action :find_movie
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:movie_id])
+end
