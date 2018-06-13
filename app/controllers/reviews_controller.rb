51cc1d33832a330a78f407625fc95118be4db445
github username <github email address>
2018-06-13 13:31:41 -0400

    comment and rating editing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_movie
+  before_action :find_review, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -7,12 +8,32 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.movie_id = @movie.id
+    @review.user_id = current_user.id
 
     if @review.save
       redirect_to movie_path(@movie)
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to movie_path(@movie)
   end
 
   private
@@ -23,4 +44,9 @@ class ReviewsController < ApplicationController
 
     def find_movie
       @movie = Movie.find(params[:movie_id])
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
+    end
 end
